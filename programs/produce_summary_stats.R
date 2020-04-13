3f7d2c8c0705b0f0bbc7cb076a4be2e8cf44a10e
Christopher Davis <cdavis@urban.org>
2020-04-13 10:29:05 -0400

    update summary stats

--- programs/produce_summary_stats.R
+++ programs/produce_summary_stats.R
@@ -2,14 +2,18 @@ library(tidyverse)
 library(sf)
 
 
-x = st_read("C:/Users/anarayanan/Downloads/job_loss_by_tract.geojson")
+job_loss <-  st_read("data/processed-data/s3_final/job_loss_by_tract.geojson")
 
-y= x %>% janitor::clean_names() %>% 
-  pivot_longer(cols = agriculture_forestry_fishing_and_hunting:public_administration, 
+job_loss_long <- job_loss %>%
+  janitor::clean_names() %>% 
+  st_drop_geometry() %>%
+  pivot_longer(cols = x01:x20, 
                names_to= "job_type", 
-               values_to = "job_loss")
+               job_losss_to = "job_loss")
 
-job_type_y %>% select(cbsa_name, job_loss_index, job_type, job_loss) %>% 
+job_loss_long %>% 
+  select(job_type, 
+         job_loss) %>% 
   group_by(job_type) %>% 
   summarize(min = min(job_loss, na.rm =T),
             quartile_0.25 = quantile(job_loss,0.25, na.rm =T),
@@ -20,5 +24,24 @@ job_type_y %>% select(cbsa_name, job_loss_index, job_type, job_loss) %>%
             )
 
 
-summary(x$job_loss_index)
-hist(x$job_loss_index)
+summary(job_loss$X000)
+hist(job_loss$X000)
+
+
+job_loss_long %>% 
+  group_by(job_type) %>%
+  mutate(job_loss_cat = case_when(
+    job_loss >= 0 & job_loss < 50 ~ "0-49",
+    job_loss >= 50 & job_loss < 100 ~ "50-99",
+    job_loss >= 100 & job_loss < 150 ~ "100-149",
+    job_loss >= 150 & job_loss < 200 ~ "150-199",
+    job_loss >=200 & job_loss < 250 ~ "200-249",
+    job_loss >=250 ~ "250+") %>% 
+    factor(levels = c("0-49", 
+                      "50-99", 
+                      "100-149", 
+                      "150-199", 
+                      "200-249", 
+                      "250+"))) %>%
+  count(job_loss_cat)  %>% 
+  write_csv("data/processed-data/summary_by_industry.csv")
