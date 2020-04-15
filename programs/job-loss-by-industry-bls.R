6830f129e775da970e4de5893450ea78d2642c31
Ajjit Narayanan <34085716+ajjitn@users.noreply.github.com>
2020-04-15 10:55:47 -0400

    Modify comment headers for script 2a and 3

--- programs/job-loss-by-industry-bls.R
+++ programs/job-loss-by-industry-bls.R
@@ -4,16 +4,21 @@ library(tidyverse)
 library(jsonlite)
 library(testit)
 
-# Parameters for start month and year from which to compare job change
+##----Set Parameters----------------------------------------------------
+# start_month month from which to compare job change
+# start_year: year from which to compare job change
 start_month <- 2
 start_year <- 2020
 
-# Read in data
+##----Read in Data------------------------------------------------------
 # Read in crosswalk from CES NAICS to LODES
+# This crosswalk was manually compiled
 lodes_crosswalk <- fromJSON("data/raw-data/small/ces-to-naics.json")
 
 # Read in all CES data
 ces_all <- read_tsv("data/raw-data/big/ces_all.txt")
+
+##----Generate % Change in employment-----------------------------------
 
 # Specify only the series we are interested in:
 # CES (seasonally adjusted) + {industry code from crosswalk} + 
@@ -49,7 +54,6 @@ ces_series_reference <- ces_series_subset_m %>%
                           filter(month == start_month) 
 
 # Function to remove and rename columns to prep for join
-#Chris note - altered slightly as this function was giving me error: "All arguments must be named"
 join_prep <- function(df, col_name){
   df %>% 
     select(series_id, value) %>%
@@ -60,7 +64,7 @@ join_prep <- function(df, col_name){
 ces_series_latest <- join_prep(ces_series_latest, "latest")
 ces_series_reference <- join_prep(ces_series_reference, "reference")
 
-# Join data together and calculate change
+# Join data together and calculate % change
 job_change <- ces_series_reference %>%
               left_join(ces_series_latest, by = "series_id") %>%
               mutate(percent_change_employment = latest / reference - 1)
@@ -75,7 +79,7 @@ job_change_led <- job_change %>%
                                             map_chr(map_supersector)) %>%
                     arrange(lodes_var)
 
-# Write out
+##----Write out data------------------------------------------------
 job_change_led %>% 
   write_csv(
     str_glue("data/processed-data/job_change_bls_{start_year}_{start_month}_to_{latest_year}_{latest_month}.csv")
