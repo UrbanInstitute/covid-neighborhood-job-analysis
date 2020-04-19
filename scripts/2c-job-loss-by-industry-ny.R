0851f7cd45bf6615cbb67830deb97822d5117b60
Graham MacDonald <GMacDonald@urban.org>
2020-04-19 12:06:55 -0400

    Passing lintrs

--- scripts/2c-job-loss-by-industry-ny.R
+++ scripts/2c-job-loss-by-industry-ny.R
@@ -8,7 +8,7 @@ library(readxl)
 ##----Set Parameters----------------------------------------------------
 # start_quarter: quarter from which to compare job change and
 # past_unemp_weeks: # of past weeks of unemployment data to use
-# filename: filename of WA unemployment data (downloaded from 
+# filename: filename of WA unemployment data (downloaded from
 #           download-data.R)
 start_quarter <- 3
 past_unemployment_weeks <- 4
@@ -36,22 +36,24 @@ lehd_types <- read_csv("data/raw-data/small/lehd_types.csv")
 ##----Get total_employment by LODES industry code-------------------------
 # Subset to NAICS supersector and latest quarter
 qcew_sub <- qcew_all %>%
-              filter(agglvl_code == 54, 
+              filter(agglvl_code == 54,
                     qtr == start_quarter)
 
 # Aggregate all employment, taking last month of the quarter,
 # By industry by supersector
-qcew_agg <- qcew_sub %>% 
+qcew_agg <- qcew_sub %>%
               group_by(industry_code) %>%
               summarize(total_employment = sum(month3_emplvl)) %>%
               filter(industry_code %in% names(lodes_crosswalk))
                   
 # Attach LODES codes "CNS{number}" and write out
-add_lodes_code <- function(industry_code){
-  str_glue("CNS",lodes_crosswalk[industry_code][[1]])
+add_lodes_code <- function(industry_code) {
+  str_glue("CNS", lodes_crosswalk[industry_code][[1]])
 }
 qcew_led <- qcew_agg %>%
-              mutate(lodes_var = qcew_agg$industry_code %>% map_chr(add_lodes_code))
+              mutate(lodes_var =
+                       qcew_agg$industry_code
+                          %>% map_chr(add_lodes_code))
 
 
 
@@ -61,20 +63,20 @@ qcew_led <- qcew_agg %>%
 # WA split
 # Calculate WA ratio of job loss
 wa_data <- read_csv("data/processed-data/job_change_wa_most_recent.csv")
-utilities_wa <- wa_data %>% 
-                  filter(industry_code == "22") %>% 
-                  select(unemployment_totals) %>% 
+utilities_wa <- wa_data %>%
+                  filter(industry_code == "22") %>%
+                  select(unemployment_totals) %>%
                   pull()
-construction_wa <- wa_data %>% 
-                    filter(industry_code == "23") %>% 
-                    select(unemployment_totals) %>% 
+construction_wa <- wa_data %>%
+                    filter(industry_code == "23") %>%
+                    select(unemployment_totals) %>%
                     pull()
 cu_ratio <- utilities_wa / (utilities_wa + construction_wa)
 
 # Add separate NY rows for constrution and utilities
 weekly_unemployment <- read_excel(str_glue("data/raw-data/small/{filename}"),
                                   sheet = "Sheet1")
-split_base <- weekly_unemployment %>% 
+split_base <- weekly_unemployment %>%
                   filter(`2 Digit NAICS` == "22, 23") %>%
                   select_if(is.numeric) %>%
                   unname()
@@ -82,21 +84,22 @@ utilities_ny <- split_base %>%
                   map_dbl(~ .x * cu_ratio)
 construction_ny <- split_base %>%
                   map_dbl(~ .x * (1 - cu_ratio))
-weekly_unemployment <- weekly_unemployment %>% 
-                        rbind(c(c("Utilities","22"), utilities_ny)) %>%
-                        rbind(c(c("Construction","23"), construction_ny)) %>%
+weekly_unemployment <- weekly_unemployment %>%
+                        rbind(c(c("Utilities", "22"), utilities_ny)) %>%
+                        rbind(c(c("Construction", "23"), construction_ny)) %>%
                         filter(`2 Digit NAICS` != "22, 23")
   
-add_naics_super_lodes <- function(naics){
-  str_glue("CNS",lodes_crosswalk_sector[naics][[1]])
+add_naics_super_lodes <- function(naics) {
+  str_glue("CNS", lodes_crosswalk_sector[naics][[1]])
 }
 weekly_unemployment_sub <- weekly_unemployment %>%
-                        mutate(lodes_var = weekly_unemployment$`2 Digit NAICS` %>% map_chr(add_naics_super_lodes)) %>%
+                        mutate(lodes_var = weekly_unemployment$`2 Digit NAICS` %>%
+                                 map_chr(add_naics_super_lodes)) %>%
                         filter(lodes_var != "CNS00")
 cols <- length(colnames(weekly_unemployment_sub))
 weekly_unemployment_sub <- weekly_unemployment_sub %>%
                             #only keep unemp claims from last n weeks
-                            select(c((cols-past_unemployment_weeks):cols)) %>%
+                            select(c((cols - past_unemployment_weeks):cols)) %>%
                             mutate_at(vars(-lodes_var), as.numeric)
 
 # Sum across rows to get total unemployment over past n weeks
@@ -104,13 +107,13 @@ weekly_unemployment_sub <- weekly_unemployment_sub %>%
 weekly_unemployment_totals <- weekly_unemployment_sub %>%
                                 data.frame(unemployment = rowSums(weekly_unemployment_sub[1:past_unemployment_weeks])) %>%
                                 select(lodes_var, unemployment) %>%
-                                # AN: Any reasom you're selecting by index rather than by 
+                                # AN: Any reasom you're selecting by index rather than by
                                 # select(c((past_unemployment_weeks+1):(past_unemployment_weeks+2))) %>%
                                 group_by(lodes_var) %>%
-                                summarize(unemployment_totals = sum(unemployment)) %>% 
+                                summarize(unemployment_totals = sum(unemployment)) %>%
                                 left_join(lehd_types %>%
                                             transmute(lodes_var = toupper(lehd_var),
-                                                      lehd_name), 
+                                                      lehd_name),
                                           by = c("lodes_var"))
 
 
@@ -119,11 +122,11 @@ weekly_unemployment_totals <- weekly_unemployment_sub %>%
 # job change in the short term until we get BLS CES data
 percent_change_industry <- qcew_led %>%
                             left_join(weekly_unemployment_totals, by = "lodes_var") %>%
-                            select(lodes_var, everything()) %>% 
+                            select(lodes_var, everything()) %>%
                           # merge(weekly_unemployment_totals, by = "lodes_var") %>%
                             mutate(percent_change_employment = -unemployment_totals / total_employment) %>%
                             arrange(lodes_var) %>%
-                            select(lehd_name, lodes_var, everything()) %>% 
+                            select(lehd_name, lodes_var, everything()) %>%
                             write_csv(str_glue("data/processed-data/job_change_ny_last_{past_unemployment_weeks}_weeks.csv"))
-percent_change_industry %>% 
+percent_change_industry %>%
   write_csv("data/processed-data/job_change_ny_most_recent.csv")
--- scripts/2z-job-loss-by-industry-combine-states.R
+++ scripts/2z-job-loss-by-industry-combine-states.R
@@ -13,12 +13,13 @@ path <- "data/processed-data/job_change_{.x}_most_recent.csv"
 ##----Calculate average-------------------------------------------------
 # Read in all files and calculate the weighted average job loss
 states_paths <- map_chr(states, ~ str_glue(path))
-all_files <-  states_paths %>% 
+all_files <-  states_paths %>%
                 map_df(~read_csv(.))
 all_files %>%
   group_by(lehd_name, lodes_var, industry_code) %>%
   summarise(total_employment = sum(total_employment),
             unemployment_totals = sum(unemployment_totals)) %>%
-  mutate(percent_change_employment = -unemployment_totals / total_employment) %>%
+  mutate(percent_change_employment =
+           -unemployment_totals / total_employment) %>%
   arrange(lodes_var) %>%
   write_csv("data/processed-data/job_change_all_states_most_recent.csv")
