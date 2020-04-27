a415297c5e9b613ad7dd0c2359e809cc294cf8ca
Graham MacDonald <GMacDonald@urban.org>
2020-04-27 15:56:21 -0400

    Changes to test for May 8 release to methodology

--- scripts/update/2z-job-loss-by-industry-by-state-update.R
+++ scripts/update/2z-job-loss-by-industry-by-state-update.R
@@ -12,7 +12,7 @@ library(testit)
 # is_bls: Using state data or BLS data for industry data
 past_unemployment_weeks <- 5
 filename <- "initial-claims-bls-state.xlsx"
-is_bls <- FALSE
+is_bls <- TRUE
 
 ##----Read in data------------------------------------------------------
 # Read in BLS QCEW data
@@ -24,7 +24,11 @@ state_claims <- read_excel(str_glue("data/raw-data/small/{filename}"),
                            sheet = "Sheet1")
 
 # Read in national industry estimates
-national_industry_est <- read_csv("data/processed-data/job_change_all_states_most_recent.csv")
+if (is_bls){
+  national_industry_est <- read_csv("data/processed-data/job_change_bls_most_recent.csv")
+} else {
+  national_industry_est <- read_csv("data/processed-data/job_change_all_states_most_recent.csv")
+}
 
 # Read in FIPS codes
 fips_codes <- read_excel("data/raw-data/big/fips.xlsx",
@@ -54,25 +58,27 @@ state_job_loss <- state_unemp %>%
 assert(nrow(state_job_loss) == 51)
 
 ##----Create state job loss by industry figures-------------------------
-# Calculate the national job loss percentage to compare to states
-national_job_loss_calc <- national_industry_est %>%
-  summarise(tot_emp = sum(total_employment), 
-            unemp_tot = sum(unemployment_totals)) %>%
-  mutate(percent_change_employment = -unemp_tot / tot_emp) %>%
+
+# Calculate the factor to adjust state data to national job loss
+national_job_loss_factor <- state_job_loss %>%
+  summarise(unemployment_totals = sum(unemployment_totals),
+            total_employment = sum(total_employment)) %>%
+  mutate(percent_change_employment = -unemployment_totals / total_employment) %>%
   select(percent_change_employment) %>%
   pull()
 
-# Calculate the factor to adjust WA, NY, etc. to national job loss
 if (!is_bls){
-  national_job_loss_factor <- state_job_loss %>%
-    summarise(unemployment_totals = sum(unemployment_totals),
-              total_employment = sum(total_employment)) %>%
-    mutate(percent_change_employment = -unemployment_totals / total_employment) %>%
+  # Calculate the national job loss percentage to compare to states
+  national_job_loss_calc <- national_industry_est %>%
+    summarise(tot_emp = sum(total_employment), 
+              unemp_tot = sum(unemployment_totals)) %>%
+    mutate(percent_change_employment = -unemp_tot / tot_emp) %>%
     select(percent_change_employment) %>%
     pull()
+  
   national_job_loss_calc_ratio <- national_job_loss_factor / (national_job_loss_calc**2)
 } else {
-  national_job_loss_calc_ratio <- 1 / national_job_loss_calc
+  national_job_loss_calc_ratio <- 1 / national_job_loss_factor
 }
 
 # Function that takes state as input, and returns a dataframe with the
@@ -121,6 +127,17 @@ if (!is_bls){
   }
   state_job_estimates <- replace_state("Washington", "53", wa)
   state_job_estimates <- replace_state("New York", "36", ny)
+} else {
+  # If values are positive job change, correct usign the inverse
+  jobs_negative <- state_job_estimates %>%
+    filter(percent_change_employment < 0)
+  jobs_positive <- state_job_estimates %>%
+    filter(percent_change_employment >= 0) %>%
+    mutate(factor = 1 / factor,
+           percent_change_employment_st = percent_change_employment * factor)
+  state_job_estimates <- jobs_negative %>%
+    rbind(jobs_positive) %>%
+    arrange(state, lodes_var)
 }
 
 # Write
