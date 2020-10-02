5ef09fd0d33ac11cb2502b9adc775e402021782d
Ajjit Narayanan <ajjit123@gmail.com>
2020-10-02 09:59:14 -0400

    Rerun with latest weeks data

--- scripts/update-master.R
+++ scripts/update-master.R
@@ -15,7 +15,7 @@ dryrun <- FALSE
 # Set bin values for legend bins and histograms. The histogram needs to be
 # manually reviewed and may need to change these values
 tmax_bins <- c(100, 150, 200, 250, 675)
-max_bins <- c(100, 250, 500, 750, 1000, 2000, 5000, 10000, 225000)
+max_bins <- c(100, 250, 500, 750, 1000, 2000, 5000, 10000, 200000)
 
 
 
@@ -27,17 +27,6 @@ if (static) {
         source("scripts\\static\\4-transfer-to-s3-static.R", encoding = "UTF-8")
     }
 }
-
-source("scripts\\update\\1-download-data-update.R", encoding = "UTF-8")
-source("scripts\\update\\2v4a-job-loss-projected-forward-ces.R", encoding = "UTF-8")
-source("scripts\\update\\2v4b-job-loss-by-industry-ces-sae-ipums-update.R", encoding = "UTF-8")
-source("scripts\\update\\2v4c-job-loss-by-industry-ipums-update.R", encoding = "UTF-8")
-source("scripts\\update\\2v4d-job-loss-by-industry-ipums-summary-update.R", encoding = "UTF-8")
-source("scripts\\update\\3-produce-data-files-update.R", encoding = "UTF-8")
-source("scripts\\update\\4-produce-summary-stats-update.R", encoding = "UTF-8")
-
-# stop because you need to review the histograms and confirm legend bounds
-stop()
 source("scripts\\update\\5-create-sum-files-update.R", encoding = "UTF-8")
 if (!dryrun) {
     source("scripts\\update\\6-transfer-to-s3-update.R", encoding = "UTF-8")
